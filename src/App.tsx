fb027ff88478b7dafa5b504d90f460923e3e4171
Joshua Lehman <joshlehman.dev@gmail.com>
2024-07-19 11:49:07 -0400

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import { Typography } from "@mui/material";
+import BookList from "./BookList";
 
 function App() {
+  const books = [{ name: "Refactoring" }, { name: "Domain-driven design" }];
+
   return (
     <>
       <Typography variant="h2" component="h2" data-test="heading">
         Bookish
       </Typography>
-      <div data-test="book-list">
-        <div className="book-item">
-          <h2 className="title">Refactoring</h2>
-        </div>
-        <div className="book-item">
-          <h2 className="title">Domain-driven design</h2>
-        </div>
-      </div>
+      <BookList books={books} />
     </>
   );
 }
