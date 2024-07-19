6dbff2cef8034636e7da3cb885e5d1d8c8af3371
Joshua Lehman <joshlehman.dev@gmail.com>
2024-07-18 22:06:55 -0400

    e2e-test

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import { Typography } from "@mui/material";
 
 function App() {
   return (
-    <Typography variant="h2" component="h2" data-test="heading">
-      Bookish
-    </Typography>
+    <>
+      <Typography variant="h2" component="h2" data-test="heading">
+        Bookish
+      </Typography>
+      <div data-test="book-list">
+        <div className="book-item"></div>
+        <div className="book-item"></div>
+      </div>
+    </>
   );
 }
 
